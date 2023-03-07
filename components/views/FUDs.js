d231051fc53cbaf8bcb6418adbcfa537267668b1
Spurlingcd <joiner.christina@gmail.com>
2023-03-07 18:38:14 -0500

    broken pages and dropdown

--- components/views/FUDs.js
+++ components/views/FUDs.js
@@ -1,22 +1,7 @@
 import html from "html-literal";
 
 export default () => html`
-  <section id="FUDs" class="page-content">
+  <section id="fuds" class="page-content">
     <h2>Funnies</h2>
-
-    <div id="moodTracker">
-      <fieldset class="mood">
-        <legend>How is your mood today?</legend>
-        <label
-          ><input type="radio" value="frown" name="tracker" /> Not So Good
-        </label>
-        <label
-          ><input type="radio" value="meh" name="tracker" /> Just... Meh
-        </label>
-        <label
-          ><input type="radio" value="fud" name="tracker" /> Frown Upside Down
-        </label>
-      </fieldset>
-    </div>
   </section>
 `;
