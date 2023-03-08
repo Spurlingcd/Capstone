8938aa2c825657fa64007195c3f0a95dc8bda2bd
Spurlingcd <joiner.christina@gmail.com>
2023-03-08 18:52:48 -0500

    Pages work and weeding out

--- components/Nav.js
+++ components/Nav.js
@@ -4,15 +4,7 @@ export default links => html`
   <nav role="navigation">
     <div class="main-nav">
       <i class="fas fa-bars"></i>
-      <ul class="hidden-mobile">
-        <!-- <li class="dropdown-content"><a href="/Home" title="Home" data-navigo>Home</a>
-      </li>
-      <li class="dropdown-content"><a href="/About" title="About" data-navigo>About</a>
-      </li>
-      <li class="dropdown-content"><a href="/FUDs" title="FUDs" data-navigo>FUDs</a>
-      <li class="dropdown-content"><a href= "/ContactUs" title="Contact Us" data-navigo>Contact Us</a>
-      </li> -->
-      </ul>
+      <ul class="hidden-mobile"></ul>
 
       ${links
         .map(
