a059b6d7de685df7f03fd5dc9f20bc654b1b801e
Spurlingcd <joiner.christina@gmail.com>
2023-03-10 16:37:23 -0500

    FUDs

--- components/views/FUDs.js
+++ components/views/FUDs.js
@@ -3,6 +3,7 @@ import html from "html-literal";
 export default () => html`
   <section id="fuds" class="page-content">
     <h2>Affirmations</h2>
+
     <h2>Funnies</h2>
     <h2>Scriptures</h2>
   </section>
