9ae78ef056591beea79b5f9bc7cd1026e84a2912
Spurlingcd <joiner.christina@gmail.com>
2023-03-09 21:03:03 -0500

    added CORs middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
+
+app.use(cors);
 app.use(express.json());
-// app.use(logging);
+app.use(logging);
 
 app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
