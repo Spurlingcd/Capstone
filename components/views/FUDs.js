e45c33128fe111f90f2d5ec8d1714e73ac35d8cb
Spurlingcd <joiner.christina@gmail.com>
2023-03-11 16:25:29 -0500

    we're deleting stuff

--- components/views/FUDs.js
+++
@@ -1,9 +0,0 @@
-import html from "html-literal";
-
-export default () => html`
-  <section id="fuds" class="page-content">
-    <h3 class="content">Affirmations</h3>
-    <h3 class="content">Funnies</h3>
-    <h3 class="content">Scriptures</h3>
-  </section>
-`;
--- store/FUDs.js
+++
@@ -1,4 +0,0 @@
-export default {
-  header: "Fuds page",
-  view: "Fuds"
-};
